333d8feb2ccdfa65e63cc5d5ceac5edb66e23007
Danielk84 <daniel-k84@outlook.com>
2025-09-25 15:06:54 +0330

    update (db/queries/users.go): adding basic json tags and improving UserStore pattern

--- db/queries/users.go
+++ db/queries/users.go
@@ -17,12 +17,12 @@ const (
 )
 
 type User struct {
-	ID             int32
-	Username       string
-	Email          *string
-	PasswordHash   *string
-	PermissionType PermissionType
-	IsActive       bool
+	ID             int32          `json:"id"`
+	Username       string         `json:"username"`
+	Email          *string        `json:"email"`
+	PasswordHash   *string        `json:"-"`
+	PermissionType PermissionType `json:"permission-type"`
+	IsActive       bool           `json:"is_active"`
 }
 
 type UserRepository struct {
@@ -32,10 +32,10 @@ type UserRepository struct {
 type UserStore interface {
 	IsUsernameExists(ctx context.Context, username string) bool
 	Create(ctx context.Context, user *User) error
-	SetEmail(ctx context.Context, user *User) error
 	Get(ctx context.Context, username string) (*User, error)
 	Update(ctx context.Context, user *User) error
 	Delete(ctx context.Context, id int32) error
+	SetEmail(ctx context.Context, user *User) error
 }
 
 func NewUserStore(session db.Session) UserStore {
@@ -63,28 +63,18 @@ func (ur *UserRepository) Create(ctx context.Context, user *User) error {
 	return execOne(ctx, ur.session, q, args)
 }
 
-func (ur *UserRepository) SetEmail(ctx context.Context, user *User) error {
-	const q = `UPDATE ONLY users SET email = @Email WHERE id = @ID`
-	args := pgx.NamedArgs{
-		"Email": user.Email,
-		"ID":    user.ID,
-	}
-	return execOne(ctx, ur.session, q, args)
-}
-
 func (ur *UserRepository) Get(ctx context.Context, username string) (*User, error) {
 	const readUserQuery = `SELECT id, username, email, password_hash, permission_type, is_active FROM users
-		WHERE username = $1
-		LIMIT 1`
+	WHERE username = $1
+	LIMIT 1`
 	return get[User](ctx, ur.session, readUserQuery, username)
 }
 
 func (ur *UserRepository) Update(ctx context.Context, user *User) error {
 	const q = `UPDATE users
-		SET password_hash = @PasswordHash, permission_type = @PermissionType, is_active = @IsActive
+		SET permission_type = @PermissionType, is_active = @IsActive
 		WHERE id = @ID`
 	args := pgx.NamedArgs{
-		"PasswordHash":   user.PasswordHash,
 		"PermissionType": user.PermissionType,
 		"IsActive":       user.IsActive,
 		"ID":             user.ID,
@@ -95,4 +85,13 @@ func (ur *UserRepository) Update(ctx context.Context, user *User) error {
 func (ur *UserRepository) Delete(ctx context.Context, id int32) error {
 	const q = `DELETE FROM users WHERE id = $1`
 	return execOne(ctx, ur.session, q, id)
+}
+
+func (ur *UserRepository) SetEmail(ctx context.Context, user *User) error {
+	const q = `UPDATE ONLY users SET email = @Email WHERE id = @ID`
+	args := pgx.NamedArgs{
+		"Email": user.Email,
+		"ID":    user.ID,
+	}
+	return execOne(ctx, ur.session, q, args)
 }
