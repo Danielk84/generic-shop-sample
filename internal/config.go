bbd2efb021fd3586740f4d0301bd8393ddb7a846
Danielk84 <daniel-k84@outlook.com>
2026-06-09 17:29:23 +0330

    refactor (internal/config.go): move older dotenv config to yaml file

--- internal/config.go
+++ internal/config.go
@@ -3,102 +3,80 @@ package internal
 import (
 	"fmt"
 	"os"
-	"os/user"
 	"path/filepath"
-	"strconv"
-	"strings"
-	"sync"
 	"time"
 
-	"github.com/joho/godotenv"
+	"go.yaml.in/yaml/v4"
 )
 
 type Config struct {
-	Mode, Addr              string
-	TrustedProxies, Origins []string
-	DatabaseURL, CacheURL   string
-	JWTSecretKey            []byte
-	Pagination              int
-	MaxMultipartMemory      int64
-	AllowedImgMimetype      []string
-	UploadPath              string
-	MaxProductImagesAmount  int
-	AuthExpiration          time.Duration
-	RequestLoggerFilepath   string
-	AppLoggerFilepath       string
-	ZPMerchantID            string
-	PaymentCallbackURL      string
-	FromEmail               string
-	SMTPHost                string
-	SMTPPort                int
-	SMTPPassword            string
+	Mode                   string        `yaml:"mode" binding:"required,oneof=debug release"`
+	Addr                   string        `yaml:"addr" binding:"required"`
+	TrustedProxies         []string      `yaml:"trusted_proxies" binding:",required,ip"`
+	Origins                []string      `yaml:"origins" binding:"required,origin"`
+	DatabaseURL            string        `yaml:"basebase_url" binding:"required,url"`
+	CacheURL               string        `yaml:"cache_url" binding:"required,url"`
+	JWTSecretKey           []byte        `yaml:"jwt_secret_key" binding:"required,ascii"`
+	Pagination             int           `yaml:"pagination" binding:"required,min=0"`
+	MaxMultipartMemory     int64         `yaml:"max_multipart_memory" binding:"required,min=0"`
+	AllowedImgMimetype     []string      `yaml:"allowed_img_mimetype" binding:"required,mimetype=image/*"`
+	UploadPath             string        `yaml:"upload_path" binding:"required,filepath"`
+	MaxProductImagesAmount int           `yaml:"max_product_image_amount" binding:"required,min=5"`
+	AuthExpiration         time.Duration `yaml:"auth_expiration" binding:"required,min=15m"`
+	RequestLoggerFilepath  string        `yaml:"request_logger_filepath" binding:"required,filepth"`
+	AppLoggerFilepath      string        `yaml:"app_logger_filepath" binding:"required,filepath"`
+	ZPMerchantID           string        `yaml:"zp_merchant_id" binding:"required"`
+	PaymentCallbackURL     string        `yaml:"payment_callback_url" binding:"required,http_url"`
+	FromEmail              string        `yaml:"from_email" binding:"required,email"`
+	SMTPHost               string        `yaml:"smtp_host" binding:"required,hostname"`
+	SMTPPort               int           `yaml:"smtp_port" binding:"required,port"`
+	SMTPPassword           string        `yaml:"smtp_password" binding:"omitempty"`
 }
 
-var (
-	DefaultConfig *Config
-	once          sync.Once
-)
-
-func NewConfig() *Config {
-	once.Do(func() {
-		args, err := godotenv.Read(getDotEnvFilePath())
-		if err != nil {
-			panic(fmt.Errorf("failed to read .env file, %s", err))
-		}
-
-		DefaultConfig = &Config{
-			Mode:                   args["MODE"],
-			Addr:                   args["ADDR"],
-			TrustedProxies:         getStrSliceFromStr(args["TRUSTED_PROXIES"]),
-			Origins:                getStrSliceFromStr(args["ORIGINS"]),
-			DatabaseURL:            args["DATABASE_URL"],
-			CacheURL:               args["CACHE_URL"],
-			JWTSecretKey:           []byte(args["JWT_SECRET_KEY"]),
-			Pagination:             getNumberFromStr(args["PAGINATION"]),
-			MaxMultipartMemory:     int64(getNumberFromStr(args["MAX_MULTIPART_MEMORY"])),
-			AllowedImgMimetype:     getStrSliceFromStr(args["ALLOWED_IMAGE_MIMETYPE"]),
-			UploadPath:             expendPath(args["UPLOAD_PATH"]),
-			MaxProductImagesAmount: getNumberFromStr(args["MAX_PRODUCT_IMAGES_AMOUNT"]),
-			AuthExpiration:         time.Duration(getNumberFromStr(args["AUTH_EXPIRATION"])),
-			RequestLoggerFilepath:  expendPath(args["REQUEST_LOGGER_FILEPATH"]),
-			AppLoggerFilepath:      expendPath(args["APP_LOGGER_FILEPATH"]),
-			ZPMerchantID:           args["ZP_MERCHANT_ID"],
-			PaymentCallbackURL:     args["PAYMENT_CALLBACK_URL"],
-			FromEmail:              args["FROM_EMAIL"],
-			SMTPHost:               args["SMTP_HOST"],
-			SMTPPort:               getNumberFromStr(args["SMTP_PORT"]),
-			SMTPPassword:           args["SMTP_PASSWORD"],
-		}
-	})
-	return DefaultConfig
+type ConfigFile struct {
+	Conf Config `yaml:"conf"`
 }
 
-func getDotEnvFilePath() string {
-	if env := os.Getenv("DOTENV_PATH"); env != "" {
-		return env
+func (c *ConfigFile) ReadFile(fp string) error {
+	if filepath.IsAbs(fp) {
+		return fmt.Errorf("%s: empty or not absolute file path", os.ErrInvalid)
 	}
-	return ".env"
-}
-
-func getStrSliceFromStr(s string) []string {
-	return strings.Split(strings.ReplaceAll(s, " ", ""), ",")
-}
-
-func getNumberFromStr(arg string) int {
-	num, err := strconv.Atoi(arg)
+	yamlFile, err := os.ReadFile(fp)
 	if err != nil {
-		panic(fmt.Errorf(`invalid arg number "%s", %s\n`, arg, err))
+		return err
 	}
-	return num
+	if err = yaml.Unmarshal(yamlFile, c); err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func expendPath(fp string) string {
-	if strings.HasPrefix(fp, "~/") {
-		user, err := user.Current()
-		if err != nil {
-			panic(fmt.Errorf(`failed to get current user path from "%s"`, fp))
-		}
-		return filepath.Join(user.HomeDir, fp[2:])
+func (c *ConfigFile) Validate() error {
+	if err := GetValidator().Struct(c.Conf); err != nil {
+		return err
 	}
-	return fp
+	return nil
+}
+
+var defualtConfig *ConfigFile
+
+func NewConfig(fp string) Config {
+	if defualtConfig == nil {
+		defualtConfig = &ConfigFile{}
+	}
+	if err := defualtConfig.ReadFile(fp); err != nil {
+		panic(err)
+	}
+	if err := defualtConfig.Validate(); err != nil {
+		panic(err)
+	}
+	return defualtConfig.Conf
+}
+
+func GetConfig() Config {
+	if defualtConfig == nil {
+		panic("nil default config")
+	}
+	return defualtConfig.Conf
 }
