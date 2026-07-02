82220f010f7317a72eacec8b156b21d7f5483f66
Danielk84 <daniel-k84@outlook.com>
2026-07-02 13:11:46 +0330

    add (client): client/src/components/common/BaseIcon.vue

---
+++ client/src/components/common/BaseIcon.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { defineAsyncComponent } from 'vue';
+
+const props = withDefaults(
+  defineProps<{
+    icon: string
+    size?: string
+    strokeColor?: string
+    fillColor?: string
+  }>(),
+  {
+    size: '24px',
+    strokeColor: "--color-base-icon",
+    fillColor: "--color-base-icon",
+  },
+)
+const icon = defineAsyncComponent(() => import(`../../assets/icons/${props.icon}`))
+
+const strokeColor = props.strokeColor == "none" ? "none" : `var(${props.strokeColor})`
+const fillColor = props.fillColor == "none" ? "none" : `var(${props.fillColor})`
+</script>
+
+<template>
+  <div>
+    <icon :style="{
+      width: props.size,
+      height: props.size,
+    }" />
+  </div>
+</template>
+
+<style scoped>
+@reference "@/styles/index.css";
+
+svg * {
+  stroke: v-bind("strokeColor");
+  fill: v-bind("fillColor");
+}
+</style>
