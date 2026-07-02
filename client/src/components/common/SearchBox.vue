3f886b8198424d098973375e538c2fa9c6d8e1a4
Danielk84 <daniel-k84@outlook.com>
2026-07-02 13:12:36 +0330

    add (client): client/src/components/common/SearchBox.vue prototype

---
+++ client/src/components/common/SearchBox.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { defineAsyncComponent } from 'vue'
+
+import icons from "@/utils/icons-list"
+
+const BaseIcon = defineAsyncComponent(() => import('@/components/common/BaseIcon.vue'))
+</script>
+
+<template>
+  <div class="base">
+    <button>
+      <BaseIcon :icon="icons.search"
+        fill-color="none"
+        stroke-color="--color-primary-color"/>
+    </button>
+    <input type="text" id="search" placeholder="search" class="search-box" />
+  </div>
+</template>
+
+<style scoped>
+@reference "@/styles/index.css";
+
+.base {
+  @apply flex flex-row items-center justify-center gap-2
+        min-w-xl h-fit p-2 rounded-xl
+        border border-navigation-bar;
+}
+
+.base input::placeholder {
+  color: var(--color-primary-color);
+}
+
+.search-box {
+  @apply w-full p-1 focus:outline-0;
+}
+</style>
